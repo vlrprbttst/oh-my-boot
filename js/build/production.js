c94cdd39628a3572f5572f86849aadb062a48e16
Valerio <omegaiori@gmail.com>
2015-09-02 12:14:23 +0200

    ga fix

--- js/build/production.js
+++ js/build/production.js
@@ -11047,7 +11047,7 @@ offsetting();
 $(window).resize(offsetting);
 
 // google analytics
-<script>
+
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -11056,5 +11056,4 @@ $(window).resize(offsetting);
   ga('create', 'UA-52919048-1', 'auto');
   ga('send', 'pageview');
 
-</script>
 
--- js/custom/main.js
+++ js/custom/main.js
@@ -103,7 +103,7 @@ offsetting();
 $(window).resize(offsetting);
 
 // google analytics
-<script>
+
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -112,5 +112,4 @@ $(window).resize(offsetting);
   ga('create', 'UA-52919048-1', 'auto');
   ga('send', 'pageview');
 
-</script>
 
