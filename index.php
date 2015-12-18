6384db9b995a8da98efa87c34f18629a60080df3
Gianzo <gianlucaazzena@hotmail.com>
2015-12-18 14:01:31 +0100

    new slide

--- index.php
+++ index.php
@@ -8,9 +8,8 @@ include 'includes/head.php';
 
 	<ul class="topslider">
 		<li>
-			<img src="images/dist/slider-top/slider-collection.jpg" class="full" />
-			<a href="http://www.ohmyboot.it/shop/" target="_blank">"<div class="overlay-text first"></div></a>
-
+			<img src="images/dist/slider-top/christmas.jpg" class="full" />
+			<a href="http://www.ohmyboot.it/shop/?page_id=1778" target="_blank"><div class="overlay-text first"></div></a>
 		</li>
 		<li>
 			<img src="images/dist/slider-top/SLIDER07.jpg" class="full" />
